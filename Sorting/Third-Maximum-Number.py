839bfb0027a37adc6044568d02a687655bdd7606
Ashnamth <ashnamth@gmail.com>
2025-07-16 20:57:25 +0330

    New Problem 3

--- Sorting/Third-Maximum-Number.py
+++ Sorting/Third-Maximum-Number.py
@@ -5,4 +5,17 @@
 
 class Solution:
     def thirdMax(self, nums: list[int]) -> int:
+        # set1 = set(nums)
+        # if len(set1) < 3:
+        #     return max(set1)
+        # nums2 = list(set1)
+        # nums2.sort()
+        # return nums2[-3]
+
+        set1 = set(nums)
+        if len(set1) < 3:
+            return max(set1)
+        for i in range(2):
+            set1.remove(max(set1))
+        return max(set1)     
         
